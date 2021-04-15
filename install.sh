42ac8f32546240c41a950c8efef317f8438cf853
eunho-lee <eunho.ambition@gmail.com>
2021-04-15 15:10:43 +0900

    only use neovim

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #! /bin/sh
 
-ln -s ~/.vim/vimrc ~/.vimrc
+#[ -f ~/.vimrc ] && rm ~/.vimrc
+#ln -s ~/.vim/vimrc ~/.vimrc
+
+[ ! -d ~/.config/ ] && mkdir ~/.config
+[ -L ~/.config/nvim ] && rm ~/.config/nvim
 ln -s ~/.vim ~/.config/nvim
+
 nvim +PlugInstall +qall now
 
 echo "Installed Vim Configuration!"
